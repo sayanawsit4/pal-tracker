f352eaab1137a6952071ec4134c51ac22761e4d5
SyamalaDora <syamala.dora@emc.com>
2019-11-19 10:07:22 +0530

    bug fix

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+//some comments
 public class PalTrackerApplication {
     public static void main(String[] args){
         SpringApplication.run(PalTrackerApplication.class, args);
